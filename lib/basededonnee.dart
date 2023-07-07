520065163ff2a686d72bd4f1c049fa3fcea78139
Moustapha0320 <123734627+Moustapha0320@users.noreply.github.com>
2023-07-07 02:33:26 +0000

    Create basededonnee.dart
    Signed-off-by: Moustapha0320 <123734627+Moustapha0320@users.noreply.github.com>

---
+++ lib/basededonnee.dart
@@ -0,0 +1,59 @@
+import 'package:mysql1/mysql1.dart';
+
+class Database {
+  static Future<MySqlConnection> connect() async {
+    final settings = ConnectionSettings(
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: '',
+      db: 'covoiturage',
+    );
+
+    return await MySqlConnection.connect(settings);
+  }
+
+  static Future<List<Map<String, dynamic>>> fetchPassagers() async {
+    final conn = await connect();
+
+    final results = await conn.query('SELECT * FROM passager');
+
+    await conn.close();
+
+    return results.map((row) => row.fields).toList();
+  }
+
+  static Future<void> insertPassager(String nom, String prenom,
+      String motDePasse, String numeroTelephone) async {
+    final conn = await connect();
+
+    await conn.query(
+      'INSERT INTO passager (nom, prenom, mot_de_passe, numero_telephone) VALUES (?, ?, ?, ?)',
+      [nom, prenom, motDePasse, numeroTelephone],
+    );
+
+    await conn.close();
+  }
+
+  static Future<List<Map<String, dynamic>>> fetchConducteurs() async {
+    final conn = await connect();
+
+    final results = await conn.query('SELECT * FROM conducteur');
+
+    await conn.close();
+
+    return results.map((row) => row.fields).toList();
+  }
+
+  static Future<void> insertConducteur(String nom, String prenom,
+      String motDePasse, String numeroTelephone, String matricule) async {
+    final conn = await connect();
+
+    await conn.query(
+      'INSERT INTO conducteur (nom, prenom, mot_de_passe, numero_telephone, matricule) VALUES (?, ?, ?, ?)',
+      [nom, prenom, motDePasse, numeroTelephone, matricule],
+    );
+
+    await conn.close();
+  }
+}
